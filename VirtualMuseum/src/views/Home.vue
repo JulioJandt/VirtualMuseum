ff0cc8645139f6745b1d0be6322c0628745caf18
JulioJandt <juliocezar.jandt@outlook.com>
2024-08-07 00:02:26 -0300

    Adiciona projeto base

---
+++ VirtualMuseum/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>Bem-vindo ao Museu Virtual</h1>
+        <v-btn color="primary" @click="$router.push('/barroco')">Explorar Barroco</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
