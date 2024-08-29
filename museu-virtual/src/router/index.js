ab7881f29e719411ecffa20776230ebe3fdd09b3
JulioJandt <juliocezar.jandt@outlook.com>
2024-08-28 23:58:19 -0300

    Ajuste para build

--- museu-virtual/src/router/index.js
+++ museu-virtual/src/router/index.js
@@ -1,13 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
 import Barroco from '../views/Barroco.vue';
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
   {
     path: '/barroco',
     name: 'Barroco',
